e88d167e697af7d987d19db934ac22dea3450d4d
conaldev <talaaidoaibiet@gmail.com>
2020-10-10 14:06:43 +0700

    them save status like button

--- src/main/java/com/xem_vn/repository/ILikeRepository.java
+++ src/main/java/com/xem_vn/repository/ILikeRepository.java
@@ -6,10 +6,14 @@ import com.xem_vn.model.Post;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ILikeRepository extends CrudRepository<Like,Long> {
     Iterable<Like> findAllByPost(Post post);
     Iterable<Like> findAllByAppUser(AppUser user);
     boolean existsByAppUserAndPost(AppUser user,Post post);
     Long countAllByPost (Post post);
     Like getByAppUserAndPost (AppUser user,Post post);
+    @Query(value = "select app_user_id from likes",nativeQuery = true)
+    List<Long> getListLikedUserIds();
 }
